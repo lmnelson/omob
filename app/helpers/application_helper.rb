5203c299260e4625c8a04ae48403b67f8737e1bd
Lee Nelson <lee@medeo.ca>
2014-09-21 22:52:24 -0700

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #convert weight to Pood
+  def pood_calc(pood)
+    #weights
+    kg = pood * 16.3806872
+    lb = pood * 36.1132336
+    weights = [kg, lb]
+  end
+
+
+
 end
